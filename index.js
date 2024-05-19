71c07fee8dda5950e2675ead38a4ed6f323d2f0e
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-19 21:48:10 +0530

    backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
@@ -20,6 +21,7 @@ app.listen(3000, () => {
 });
 
 app.use(express.json());
+app.use(cors);
 app.use(cookieParser());
 
 app.use('/api/user', userRouter);
