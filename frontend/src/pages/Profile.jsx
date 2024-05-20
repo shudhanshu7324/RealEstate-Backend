12d232abb2d1e08f8e5d3bf98222e9ceebbad355
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-21 00:40:31 +0530

    delete user bug fix

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -104,7 +104,7 @@ export default function Profile() {
       }
       dispatch(deleteUserSuccess(data))
     } catch (error) {
-      dispatch(deleteUserFailure(error.message))
+      dispatch(deleteUserFailure(error.message));
     }
   }
 
