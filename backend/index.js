07053f4c3e72f3cc43a7a467d95fb9d741529b4d
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-20 21:45:40 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use('/api/listing', listingRouter)
 app.use(express.static(path.join(__dirname,'/frontend/dist')));
 
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
 })
 
 // custom middleware for error
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -126,20 +126,22 @@ export default function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
           ref={fileRef}
           hidden
           accept="image/*"
+          
         />
         <img
           onClick={() => fileRef.current.click()}
           src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-        />
+          />
+          <span className="text-center text-xs">Upload Image</span>
         <p className="text-sm self-center">
           {fileUploadError ? (
             <span className="text-red-700 ">
