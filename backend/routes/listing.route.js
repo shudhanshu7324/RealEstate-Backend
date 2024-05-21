cc55d6711bc98a9849f4da9aeac987de38554455
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-21 12:02:20 +0530

    update listing added

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/vertifyUser.js";
 const router = express.Router();
 
 router.post('/create',verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
+
+
 
 
 
